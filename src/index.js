83891a9947c042a528c6a333978889632ba31840
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-11-27 03:19:30 +0000

    Implement email validation through JS

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import "./styles.css";
 import { generateForm } from "./forms";
 
 generateForm();
+
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an email address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
+password.addEventListener("input", (event) => {});
